203756e78f0a4f7b1fca7a97bda58384301606f5
Meinhard Kissich <meinhard.kissich@tugraz.at>
2023-02-26 21:46:18 +0100

    Test with 1500 digits

--- genmux.py
+++ genmux.py
@@ -12,7 +12,7 @@ def make_table() -> dict():
 if __name__ == "__main__":
     
     ## SETUP ##
-    NR_DIGITS = 1401
+    NR_DIGITS = 1500
     ###########
     
     assert NR_DIGITS % 3 == 0, "Number of digits must be a multiple of 3"
